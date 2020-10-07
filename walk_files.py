08d05c11c5fb3fb8bc66f159669fa2bb087fc056
jackyen0213 <71924591+jackyen0213@users.noreply.github.com>
2020-10-07 11:27:55 +0800

    Create walk_files.py

---
+++ walk_files.py
@@ -0,0 +1,51 @@
+
+import os
+import re
+import shutil
+import time
+import datetime
+import subprocess
+
+def parse_modify_file(filename):
+    filename_out = filename + "_new"
+    filename_bak = filename + "_bak"
+
+    with open(filename, "r") as fp, open(filename_out, "w") as out_fp:
+        lines = fp.readlines()
+
+        pat = re.compile(r'-lNPI')
+
+        for line in lines:
+            if re.search(pat, line):
+                print("\t$(LD) -o $@ $(OBJS) -L$(NPI_L1_DIR) -L$(NPI_LIB) -ldl -lpthread -lrt -lNPI -lnpiL1 -lz", file=out_fp)
+            else:
+                print(f"{line}", file=out_fp, end="")
+
+    shutil.copy(filename, filename_bak)
+    shutil.move(filename_out, filename)
+
+
+def walk_dir(root_dir):
+    for cur_dir, sub_dirs, files in os.walk(root_dir):
+        print(f"Current dir: {cur_dir}");
+
+        for a_file in files:
+            if a_file == "Makefile":
+                abs_filename = os.path.join(cur_dir, a_file)
+                print(f"a_file = {abs_filename}")
+                parse_modify_file(abs_filename)
+
+        for sub_dir in sub_dirs:
+            walk_dir(sub_dir)
+
+
+def main():
+    path = '/remote/vgssr1/sp_verify1/regression/VIA/via_examples/NPI_Libraries';
+    #path = '/remote/vgssr1/sp_verify1/regression/VIA/via_examples/NPI_Libraries/HierTree/npi_hier_tree_dump_txt';
+
+    walk_dir(path)
+
+
+if __name__ == "__main__":
+    main()
+
